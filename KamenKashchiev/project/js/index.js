adbe8eb0e8ca52d92679587c9cd37114b0066303
Kamen <30459127+katzata@users.noreply.github.com>
2018-03-21 11:46:51 +0200

    Project draft
    Това съм сътворил до момента. Очевидно има още за доизпипване, но вярвам че до края на курса ще се справя. Идеята на <section id="main1"> е да бъде заменяна от други секции(надявам се, че така се прави SPA). Това което е в index.js ще бъде променено изцяло(със querySelector ili getElement... + eventListener)  Със сигурност ще имам въпроси преди да приключа.

--- KamenKashchiev/project/js/index.js
+++ KamenKashchiev/project/js/index.js
@@ -4,6 +4,6 @@ cometo.src = "audio/cometo.wav";
 const threat = new Audio();
 threat.src = "audio/threat.wav";
 
-document.querySelector('#sectDimm').scrollIntoView({ 
+document.querySelector('#h1Dimm').scrollIntoView({ 
   behavior: 'smooth' 
 });
