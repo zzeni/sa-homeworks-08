590b9e44f39fb82cf4840d07fa3c3dba0015f113
Kamen <30459127+katzata@users.noreply.github.com>
2018-03-21 11:36:46 +0200

    project draft
    Това съм сътворил до момента. Очевидно има още за доизпипване, но вярвам че до края на курса ще се справя. Идеята на <section id="main1"> е да бъде заменяна от други секции(надявам се, че така се прави SPA).

---
+++ KamenKashchiev/project/js/index.js
@@ -0,0 +1,9 @@
+const cometo = new Audio();
+cometo.src = "audio/cometo.wav";
+
+const threat = new Audio();
+threat.src = "audio/threat.wav";
+
+document.querySelector('#sectDimm').scrollIntoView({ 
+  behavior: 'smooth' 
+});
